356a4045286b61d987cad0bced5c39fab0adcaa3
Jeff Silzer <jeff.silzer@biola.edu>
2016-10-19 14:58:36 -0700

    allow trustees and contractors in people search results

--- app/controllers/wcms_components/people_controller.rb
+++ app/controllers/wcms_components/people_controller.rb
@@ -22,8 +22,8 @@ class WcmsComponents::PeopleController < ApplicationController
   private
 
   def permitted_people
-    # Return all people who are either employees, faculty, or not private.
-    Person.where({'$or' => [{affiliations: 'employee'}, {affiliations: 'faculty'}, {privacy: { '$ne' => true }}] })
+    # Return all people who are either employees, faculty, trustees, contractors, or not private.
+    Person.where({'$or' => [{affiliations: 'employee'}, {affiliations: 'faculty'}, {affiliations: 'trustee'}, {affiliations: 'contractor'}, {privacy: { '$ne' => true }}] })
   end
 
   def can_search_people?
